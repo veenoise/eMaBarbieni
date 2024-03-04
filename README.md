116c139c5d5bb4393dc94ee269637655ffaeaec4
veenoise <william.eduard123@gmail.com>
2024-03-04 13:44:10 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # eMaBarbieni
-# eMaBarbieni
---
+++ scripts/content.js
@@ -0,0 +1,5 @@
+const image = document.querySelector(".navbar-brand > img");
+image.src = "https://i.imgur.com/4HZmERQ.png";
+
+const image1 = document.querySelector(".logo-footer > a > img");
+image1.src = "https://i.imgur.com/4HZmERQ.png";
